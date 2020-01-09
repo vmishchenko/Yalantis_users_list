70a41632ea3b4a216f4244cd67e0e59f9d58a417
vmishchenko <vladislav.a.mishchenko@gmail.com>
2020-01-09 18:30:07 +0200

    Updates

---
+++ precache-manifest.5e08bd17bab7fb47b32b6f3648b39ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db4af44b8dd5593db0531c59e5e3ebf",
+    "url": "/Yalantis_users_list/index.html"
+  },
+  {
+    "revision": "e87d39912f079e70211d",
+    "url": "/Yalantis_users_list/static/css/main.516c3a84.chunk.css"
+  },
+  {
+    "revision": "e749a074cc0c5240d3ff",
+    "url": "/Yalantis_users_list/static/js/2.9a928567.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Yalantis_users_list/static/js/2.9a928567.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e87d39912f079e70211d",
+    "url": "/Yalantis_users_list/static/js/main.baa9c1d6.chunk.js"
+  },
+  {
+    "revision": "45343d759d88e89e4e2c",
+    "url": "/Yalantis_users_list/static/js/runtime-main.c32ef438.js"
+  }
+]);
