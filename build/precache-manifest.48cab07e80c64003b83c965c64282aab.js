c5178be0219ae517dbc4a29c842c13e57780b1c2
vmishchenko <vladislav.a.mishchenko@gmail.com>
2020-01-09 18:26:39 +0200

    solution

---
+++ build/precache-manifest.48cab07e80c64003b83c965c64282aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6367af90427ef72266f5c3ee7e63c07c",
+    "url": "/Yalantis_users_list/index.html"
+  },
+  {
+    "revision": "b1a48f3bfc5b577fdc12",
+    "url": "/Yalantis_users_list/static/css/main.516c3a84.chunk.css"
+  },
+  {
+    "revision": "88199ea2896e4d913714",
+    "url": "/Yalantis_users_list/static/js/2.045d1c98.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Yalantis_users_list/static/js/2.045d1c98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1a48f3bfc5b577fdc12",
+    "url": "/Yalantis_users_list/static/js/main.5261ddf3.chunk.js"
+  },
+  {
+    "revision": "3997f3fe58c42be34e2f",
+    "url": "/Yalantis_users_list/static/js/runtime-main.5d42ae91.js"
+  }
+]);
---
+++ build/precache-manifest.602dbfaa64a45513a94cfdcade7bf975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d545fcb2a3d9b54e81c914e04dbc7296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc676b3722112abb623",
+    "url": "/static/css/main.516c3a84.chunk.css"
+  },
+  {
+    "revision": "88199ea2896e4d913714",
+    "url": "/static/js/2.045d1c98.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.045d1c98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dc676b3722112abb623",
+    "url": "/static/js/main.91b036cf.chunk.js"
+  },
+  {
+    "revision": "df20c98acfd77355c129",
+    "url": "/static/js/runtime-main.c74b2fe4.js"
+  }
+]);
